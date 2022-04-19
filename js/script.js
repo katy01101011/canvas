afcc2af58be9dada8076fb3cac7566542dab64c8
katy01101011 <katy.91560@gmail.com>
2022-04-19 17:16:30 +0200

    add: line in canvas

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-alert('ciao');
+const canvas = document.getElementById('canvas');
+console.log(canvas);
+
+const ctx = canvas.getContext('2d');
+ctx.strokeStyle = 'black';
+ctx.lineWidth = 1;
+
+ctx.beginPath();
+ctx.moveTo(10,5);
+ctx.lineTo(30,15);
+ctx.stroke();
